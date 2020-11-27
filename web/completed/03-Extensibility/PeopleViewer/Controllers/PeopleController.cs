04049b9fea437a128e83e64070b7fe69f740bcf1
Jeremy Clark <jeremy@jeremybytes.com>
2020-11-27 12:50:13 -0800

    Initial commit (.NET 5.0)

---
+++ web/completed/03-Extensibility/PeopleViewer/Controllers/PeopleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleViewer.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Service()
+        {
+            ViewData["Title"] = "Using a Web Service";
+            return GetPeople("Service");
+        }
+
+        public IActionResult CSV()
+        {
+            ViewData["Title"] = "Using a CSV Text File";
+            return GetPeople("CSV");
+        }
+
+        public IActionResult SQL()
+        {
+            ViewData["Title"] = "Using a SQL Database";
+            return GetPeople("SQL");
+        }
+
+
+        private IActionResult GetPeople(string readerType)
+        {
+            var reader = ReaderFactory.GetReader(readerType);
+            var people = reader.GetPeople();
+            ViewData["ReaderType"] = reader.GetType().ToString();
+            return View("Index", people);
+        }
+    }
+}
